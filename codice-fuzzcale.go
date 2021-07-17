7821b809b490faaac5a248dbb6819a50e66b12e1
tonyarris <ash552@york.ac.uk>
2021-07-17 15:11:53 +0200

    Add title and user input

--- codice-fuzzcale.go
+++ codice-fuzzcale.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/common-nighthawk/go-figure"
+)
 
 func main() {
-	fmt.Println("Testing!")
+
+	// print title
+	title := figure.NewColorFigure("codice FUZZcale", "slant", "red", true)
+	title.Print()
+	time.Sleep(1)
+	fmt.Println()
+
+	// collect known information from user
+	// TODO - implement unknown functionality
+	// prompt & store surname
+	fmt.Println("Enter surname(s) (Enter for unknown): ")
+	var surname string
+	fmt.Scanln(&surname)
+
+	// prompt & store firstname
+	fmt.Println("Enter firstname(s) (Enter for unknown): ")
+	var firstname string
+	fmt.Scanln(&firstname)
+
+	// prompt & store sex
+	fmt.Println("Enter sex (M/F/Enter for unknown): ")
+	var sex string
+	fmt.Scanln(&sex)
+
+	// prompt & store DOB
+	const (
+		layoutISO = "2006-01-02"
+	)
+	fmt.Println("Enter date of birth (yyyy-mm-dd): ")
+	var dob string
+	fmt.Scanln(&dob)
+	t, _ := time.Parse(layoutISO, dob)
+	fmt.Print(t)
+
+	// prompt & store comune
+	fmt.Println("Enter comune of birth: ")
+	var comune string
+	fmt.Scanln(&comune)
+
+	//TODO - calculate check character
+
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tonyarris/codice-fuzzcale
+
+go 1.16
+
+require github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
