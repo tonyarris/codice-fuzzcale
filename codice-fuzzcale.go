07436eaf9d6aa56dc013b40653b88ad56daf4f5a
tonyarris <ash552@york.ac.uk>
2021-07-17 16:15:00 +0200

    Add surname, name, birthyear generation

--- codice-fuzzcale.go
+++ codice-fuzzcale.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/common-nighthawk/go-figure"
@@ -21,15 +22,18 @@ func main() {
 	fmt.Println("Enter surname(s) (Enter for unknown): ")
 	var surname string
 	fmt.Scanln(&surname)
+	surname = strings.ToUpper(surname)
 
 	// prompt & store firstname
 	fmt.Println("Enter firstname(s) (Enter for unknown): ")
 	var firstname string
 	fmt.Scanln(&firstname)
+	firstname = strings.ToUpper(firstname)
 
 	// prompt & store sex
 	fmt.Println("Enter sex (M/F/Enter for unknown): ")
 	var sex string
+	sex = strings.ToUpper(sex)
 	fmt.Scanln(&sex)
 
 	// prompt & store DOB
@@ -40,13 +44,61 @@ func main() {
 	var dob string
 	fmt.Scanln(&dob)
 	t, _ := time.Parse(layoutISO, dob)
-	fmt.Print(t)
+	//fmt.Print(t)
 
 	// prompt & store comune
 	fmt.Println("Enter comune of birth: ")
 	var comune string
 	fmt.Scanln(&comune)
 
+	// Construct codice fiscale
+
+	// surname triplet
+	// remove vowels from surname
+	for _, c := range []string{"A", "E", "I", "O", "U"} {
+
+		surname = strings.ReplaceAll(surname, c, "")
+	}
+	surname = surname[0:3]
+	// TODO - account for <3 consonants & <3 letters
+
+	// name triplet
+	// remove vowels from name
+	for _, c := range []string{"A", "E", "I", "O", "U"} {
+
+		firstname = strings.ReplaceAll(firstname, c, "")
+	}
+
+	// if > 3 consonants in firstname, skip the second
+	var nameTrip []rune = []rune(firstname)
+	if len(firstname) > 3 {
+		nameTrip = delChar(nameTrip, 1)
+	}
+	firstname = string(nameTrip)
+	firstname = firstname[0:3]
+
+	// birth year
+	var birthYear int
+	birthYear = t.Year()
+	birthYear = birthYear % 100
+
+	// day of birth
+	// day counter
+	var dayCount int
+	if sex == "M" {
+		dayCount = 0
+	}
+	if sex == "F" {
+		dayCount = 40
+	}
+	dayCount = dayCount
 	//TODO - calculate check character
 
+	// print concatenated CF
+	fmt.Print(surname, firstname, birthYear)
+
+}
+
+func delChar(s []rune, index int) []rune {
+	return append(s[0:index], s[index+1:]...)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/tonyarris/codice-fuzzcale
 
 go 1.16
 
-require github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+)
