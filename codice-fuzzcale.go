1a48b44680a377d6382ef9115a72cd0bcbbe15bb
tonyarris <ash552@york.ac.uk>
2021-07-18 16:35:42 +0200

    Add check character

--- codice-fuzzcale.go
+++ codice-fuzzcale.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 
@@ -35,8 +36,8 @@ func main() {
 	// prompt & store sex
 	fmt.Println("Enter sex (M/F/Enter for unknown): ")
 	var sex string
-	sex = strings.ToUpper(sex)
 	fmt.Scanln(&sex)
+	sex = strings.ToUpper(sex)
 
 	// prompt & store DOB
 	const (
@@ -128,6 +129,8 @@ func main() {
 	}
 	cCodes := strings.Split(string(content2), "\n")
 
+	// TODO - add foreign comune codes
+
 	// create comune map
 	comuneMap := make(map[string]string)
 	for i := range cNames {
@@ -136,10 +139,147 @@ func main() {
 	}
 
 	comuneCode := comuneMap[comune]
-	//TODO - calculate check character
+
+	// calculate check character
+	var cf string = surname + firstname + strconv.Itoa(birthYear) + mCode + strconv.Itoa(day) + comuneCode
+
+	// split into evens & evens
+	runeCF := []rune(cf)
+	var odd string
+	var even string
+
+	for i := 0; i < len(runeCF); i = i + 2 {
+		odd = odd + string(runeCF[i])
+	}
+	for i := 1; i < len(runeCF); i = i + 2 {
+		even = even + string(runeCF[i])
+	}
+
+	// odd check map
+	oddMap := make(map[string]int)
+	oddMap["0"] = 1
+	oddMap["1"] = 0
+	oddMap["2"] = 5
+	oddMap["3"] = 7
+	oddMap["4"] = 9
+	oddMap["5"] = 13
+	oddMap["6"] = 15
+	oddMap["7"] = 17
+	oddMap["8"] = 19
+	oddMap["9"] = 21
+	oddMap["A"] = 1
+	oddMap["B"] = 0
+	oddMap["C"] = 5
+	oddMap["D"] = 7
+	oddMap["E"] = 9
+	oddMap["F"] = 13
+	oddMap["G"] = 15
+	oddMap["H"] = 17
+	oddMap["I"] = 19
+	oddMap["J"] = 21
+	oddMap["K"] = 2
+	oddMap["L"] = 4
+	oddMap["M"] = 18
+	oddMap["N"] = 20
+	oddMap["O"] = 11
+	oddMap["P"] = 3
+	oddMap["Q"] = 6
+	oddMap["R"] = 8
+	oddMap["S"] = 12
+	oddMap["T"] = 14
+	oddMap["U"] = 16
+	oddMap["V"] = 10
+	oddMap["W"] = 22
+	oddMap["X"] = 25
+	oddMap["Y"] = 24
+	oddMap["Z"] = 23
+
+	// even check map
+	evenMap := make(map[string]int)
+	evenMap["0"] = 0
+	evenMap["1"] = 1
+	evenMap["2"] = 2
+	evenMap["3"] = 3
+	evenMap["4"] = 4
+	evenMap["5"] = 5
+	evenMap["6"] = 6
+	evenMap["7"] = 7
+	evenMap["8"] = 8
+	evenMap["9"] = 9
+	evenMap["A"] = 0
+	evenMap["B"] = 1
+	evenMap["C"] = 2
+	evenMap["D"] = 3
+	evenMap["E"] = 4
+	evenMap["F"] = 5
+	evenMap["G"] = 6
+	evenMap["H"] = 7
+	evenMap["I"] = 8
+	evenMap["J"] = 9
+	evenMap["K"] = 10
+	evenMap["L"] = 11
+	evenMap["M"] = 12
+	evenMap["N"] = 13
+	evenMap["O"] = 14
+	evenMap["P"] = 15
+	evenMap["Q"] = 16
+	evenMap["R"] = 17
+	evenMap["S"] = 18
+	evenMap["T"] = 19
+	evenMap["U"] = 20
+	evenMap["V"] = 21
+	evenMap["W"] = 22
+	evenMap["X"] = 23
+	evenMap["Y"] = 24
+	evenMap["Z"] = 23
+
+	// calculate intermediate values
+	runeOdd := []rune(odd)
+	runeEven := []rune(even)
+	var oddValue int
+	var evenValue int
+	for i := 0; i < len(runeOdd); i++ {
+		oddValue = oddValue + oddMap[string(runeOdd[i])]
+	}
+	for i := 0; i < len(runeEven); i++ {
+		evenValue = evenValue + evenMap[string(runeEven[i])]
+	}
+	combinedValue := (oddValue + evenValue) % 26
+
+	// remainder map
+	rem := make(map[int]string)
+	rem[0] = "A"
+	rem[1] = "B"
+	rem[2] = "C"
+	rem[3] = "D"
+	rem[4] = "E"
+	rem[5] = "F"
+	rem[6] = "G"
+	rem[7] = "H"
+	rem[8] = "I"
+	rem[9] = "J"
+	rem[10] = "K"
+	rem[11] = "L"
+	rem[12] = "M"
+	rem[13] = "N"
+	rem[14] = "O"
+	rem[15] = "P"
+	rem[16] = "Q"
+	rem[17] = "R"
+	rem[18] = "S"
+	rem[19] = "T"
+	rem[20] = "U"
+	rem[21] = "V"
+	rem[22] = "W"
+	rem[23] = "X"
+	rem[24] = "Y"
+	rem[25] = "Z"
+
+	check := rem[combinedValue]
 
 	// print concatenated CF
-	fmt.Print(surname, firstname, birthYear, mCode, day, comuneCode)
+	cf = cf + check
+	fmt.Print(cf)
 
 }
 
